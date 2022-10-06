bf36a481eb1ce6b8f5c4a43fa35427d13883920b
FredB <fb6328@gmail.com>
2022-10-06 19:08:57 +0300

    Changes

--- Output_and_Escape_Sequence/Output_Escape_Sequence.cpp
+++ Output_and_Escape_Sequence/Output_Escape_Sequence.cpp
@@ -2,8 +2,8 @@
 using namespace std; // Namespace library
 int main()           // Main function. Begining point of CPU when processing this program
 {
-    cout << "Today's class \n is online via Zoom"; // Simple output of a text/string
-                                                   // cout << "Dear DBIT Class.\n";
-    // cout << "It's end-year already!\n";
+    cout << "Today's class is \t online via Zoom\n"; // Simple output of a text/string
+    cout << "Dear DBIT Class.\\r";
+    cout << "It's end-year already!\\a";
     return 0;
 }
