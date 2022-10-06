c07a85416e6a7886ffcc656bf9e9d7140d5fcfe6
FredB <fb6328@gmail.com>
2022-10-06 18:46:34 +0300

    New changes

---
+++ Output_and_Escape_Sequence/Output_Escape_Sequence.cpp
@@ -0,0 +1,9 @@
+#include <iostream>  //Preprocessor directive
+using namespace std; // Namespace library
+int main()           // Main function. Begining point of CPU when processing this program
+{
+    cout << "Today's class \n is online via Zoom"; // Simple output of a text/string
+                                                   // cout << "Dear DBIT Class.\n";
+    // cout << "It's end-year already!\n";
+    return 0;
+}
