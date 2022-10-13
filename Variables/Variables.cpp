6b0293352fe617363cd8e3b5095007ddfaf948ae
FredB <fb6328@gmail.com>
2022-10-13 22:52:34 +0300

    Update 3

--- Variables/Variables.cpp
+++ Variables/Variables.cpp
@@ -1,12 +1,14 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
 int main()
 {
     // Variable declaration
     int myNum; // Whole number without decimal
-    int age, year, phone;
-    char myName;
+    int age, year;
+    string myName, phone;
+    char gender;
     bool present;
     float temp;
     double bankBalance;
@@ -15,17 +17,21 @@ int main()
     myNum = 2;
     age = 25;
     year = 2022;
-    // myName = F;
-    // phone = 12345;
-    temp = 24.6;
-    bankBalance = 20078847.3837464646;
-    present = "True";
+    gender = 'M';
+    phone = "0710240708";
+    present = true;
+    temp = 36.01;
+    bankBalance = 2000.123456;
 
     // Display variable data
-    cout << "Number =" << myNum;
-    cout << "Age =" << age;
-    cout << "Year =" << year;
-    cout << "My name =" << myName;
-    cout << "Phone number =" << phone;
-    cout << "Age =" << age;
+    cout << "*********MY PROFILE*******";
+    cout << "Number:" << myNum << endl;
+    cout << "Age:" << age << endl;
+    cout << "Gender:" << gender << endl;
+    cout << "Current Year:" << year << endl;
+    cout << "Phone:" << phone << endl;
+    cout << "Attended class:" << present << endl;
+    cout << "My temperature:" << temp << endl;
+    cout << "My current bank balance:" << bankBalance << endl
+         << endl;
 }
