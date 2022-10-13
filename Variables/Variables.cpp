55793e62233b61a133a8e8fbffc1cf12757e7baf
FredB <fb6328@gmail.com>
2022-10-13 20:31:35 +0300

    Update

---
+++ Variables/Variables.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // Variable declaration
+    int myNum; // Whole number without decimal
+    int age, year, phone;
+    char myName;
+    bool present;
+    float temp;
+    double bankBalance;
+
+    // Variable initialization
+    myNum = 2;
+    age = 25;
+    year = 2022;
+    // myName = F;
+    // phone = 12345;
+    temp = 24.6;
+    bankBalance = 20078847.3837464646;
+    present = "True";
+
+    // Display variable data
+    cout << "Number =" << myNum;
+    cout << "Age =" << age;
+    cout << "Year =" << year;
+    cout << "My name =" << myName;
+    cout << "Phone number =" << phone;
+    cout << "Age =" << age;
+}
